90711a070626dd155d1cb1aa19b96c1a9d64f566
Hoenn <skatevan1313@gmail.com>
2015-11-02 14:34:47 -0500

    V-sync causes stuttering on laptop but not desktop, must be toggleable setting, default off

--- CurtainFire-desktop/src/com/neet/cf/desktop/DesktopLauncher.java
+++ CurtainFire-desktop/src/com/neet/cf/desktop/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.resizable=false;
-		config.vSyncEnabled=true;
+		config.vSyncEnabled=false;
 		config.width=640;
 		config.height=480;
 		new LwjglApplication(new CurtainFire(), config);
